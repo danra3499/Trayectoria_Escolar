ff7bdfbb3c9dd743bb5d258f733c67f90505883e
JorgeGarcia20 <alberto-garcia20@outlook.com>
2021-07-23 22:45:59 -0500

    Mejoras de GUI, funcion para mostrar las materias de cada semestre y adicion de las materias desde un query

--- src/models/ModeloMateria.py
+++ src/models/ModeloMateria.py
@@ -1,3 +1,6 @@
+from .entities.Materia import Materia
+
+
 class Modelo_materia():
     @classmethod
     def agregar(self, db, nombre, n_creditos, semestre):
@@ -7,5 +10,21 @@ class Modelo_materia():
             VALUES('{0}','{1}','{2}')""".format(nombre, n_creditos, semestre)
             cursor.execute(query)
             db.connection.commit()
+        except Exception as e:
+            raise Exception(e)
+
+    @classmethod
+    def obtener_materias_por_semestre(self, db, semestre):
+        try:
+            cursor = db.connection.cursor()
+            query = """SELECT nombre, n_creditos FROM materia
+            WHERE id_semestre = {0}""".format(semestre)
+            cursor.execute(query)
+            data = cursor.fetchall()
+            materias_por_semestre = []
+            for materia in data:
+                materias = Materia(None, materia[0], materia[1], None)
+                materias_por_semestre.append(materias)
+            return materias_por_semestre
         except Exception as e:
             raise Exception(e)
